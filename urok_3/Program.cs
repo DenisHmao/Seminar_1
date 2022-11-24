ad906aa04ad186b2130bafad7df0748364a8b29c
Denis <kamenhm@mail.ru>
2022-11-24 16:25:54 +0500

    new3

---
+++ urok_3/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] dataArray = new int[] { 0, 1, 2, 3, 4, 5 };
+
+int summResult = 0;
+var selectedData = dataArray.Select(
+    x =>
+    {
+        summResult += x;
+        return x;
+    });
+
+Console.WriteLine(summResult);
